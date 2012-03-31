526414b2270fee9ad1783a97018382df1a619b08
Mike Chirico <mchirico@gmail.com>
2012-03-31 08:22:53 -0400

    Updated note

--- apps/23isprime.appspot.com_Python2.7/helloworld.py
+++ apps/23isprime.appspot.com_Python2.7/helloworld.py
@@ -2,7 +2,8 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
   def get(self):
       self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write('Hello, webapp World! Version2')
+      self.response.out.write('Hello, webapp World!\nVersion3\n')
+      self.response.out.write('Source:https://github.com/mchirico/chirico/tree/master/apps/23isprime.appspot.com_Python2.7')
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
