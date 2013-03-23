87356dfed2c56f9ff92dd8e583c8c983ec27a78f
Mike Chirico <mchirico@gmail.com>
2013-03-23 07:45:00 -0400

    Auto commit for save pt: see mdo

--- c/simple_but_common/hash.c
+++ c/simple_but_common/hash.c
@@ -31,7 +31,9 @@
  deal with bucket types.
  
  
- 
+ References:
+ https://github.com/troydhanson/uthash
+ http://svn.python.org/view/python/trunk/Objects/dictobject.c?view=markup
  */
 
 
@@ -203,6 +205,27 @@ void initHashTable(HashTable ht[])
  */
 /*  End HASHTABLE */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+double Plus (double a, double b) { return a+b; }
+double Mult (double a, double b) { return a*b; }
+double Minus (double a, double b) { return a-b; }
+double Div (double a, double b) { return a/b; }
+
+void SF(double a, double b, double (*ptF)(double, double))
+{
+    double result = ptF(a,b);
+    printf("%lf\n",result);
+}
+/*
+int main()
+{
+
+    return 0;
+    
+}
+*/
 int main(int argc, const char * argv[])
 {
     HashTable ht[HASH_TABLE_SIZE];
@@ -224,7 +247,9 @@ int main(int argc, const char * argv[])
     printf("getValue(%s,ht)= %s\n","two",getValue("two",ht));
     
     
-    
+    SF(10,23,&Plus);
+    SF(10,23,&Mult);
+    SF(10,23,&Minus);
     
     return 0;
 }
